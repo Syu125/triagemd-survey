8699733f82a09278615fcdda8e94cb383ab86ee0
Syu125 <yusophia2011@gmail.com>
2026-02-11 18:20:29 -0800

    Update youtube link

--- app/instructions/page.tsx
+++ app/instructions/page.tsx
@@ -10,7 +10,7 @@ export default function Instructions() {
         <iframe
           width="100%"
           height="400"
-          src="https://www.youtube.com/embed/FUBl-vQ759U?autoplay=1"
+          src="https://www.youtube.com/embed/A7Zwq2hOiuM?autoplay=1"
           title="Instructions Video"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
